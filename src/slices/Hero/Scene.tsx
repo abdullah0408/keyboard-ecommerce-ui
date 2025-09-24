f2e92daed64075f50c9d85e05c695eaaad734b30
abdullah <abdullah981066@gmail.com>
2025-09-24 07:42:21 +0530

    feat: Enhance 3D scene with textured materials and floating keycaps
    This commit significantly enhances the visual appeal of the 3D hero scene by replacing placeholder materials with high-quality textures and introducing a dynamic floating keycap effect.
    
    The main keyboard model now features distinct materials for the case, knob, plate, and screen, each with custom textures and properties for a more realistic appearance. A new `Keycap` component has been created to render individual keycaps, which are now scattered around the main keyboard to create a sense of depth and motion. Finally, an HDR environment map has been added to provide realistic lighting and reflections.
    
    Key changes include:
    
    - Added textured materials for the keyboard case, knob, and screen.
    - Created a new `Keycap` component to display individual keycaps.
    - Added floating keycaps to the hero scene for a dynamic effect.
    - Added an HDR environment map for improved lighting and reflections.

--- src/slices/Hero/Scene.tsx
+++ src/slices/Hero/Scene.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { Keyboard } from "@/components/Keyboard";
-import { PerspectiveCamera } from "@react-three/drei";
+import { Keycap } from "@/components/Keycap";
+import { Environment, PerspectiveCamera } from "@react-three/drei";
 // import { useControls } from "leva";
 
 export function Scene() {
@@ -15,15 +16,37 @@ export function Scene() {
   //       rotationZ: 0,
   //     });
 
+  const scalingFactor = window.innerWidth <= 500 ? 0.5 : 1;
+
   return (
     <group>
       <PerspectiveCamera makeDefault position={[0, 0, 4]} fov={50} />
-      <Keyboard
-        scale={9}
-        position={[0.2, -0.5, 1.9]}
-        rotation={[1.6, 0.4, 0]}
+
+      <group scale={scalingFactor}>
+        <Keyboard
+          scale={9}
+          position={[0.2, -0.5, 1.9]}
+          rotation={[1.6, 0.4, 0]}
+        />
+
+        <group>
+          <Keycap position={[0, -0.4, 2.6]} texture={0} />
+          <Keycap position={[-1.4, 0, 2.3]} texture={1} />
+          <Keycap position={[-1.8, 1, 1.5]} texture={2} />
+          <Keycap position={[0, 1, 1]} texture={3} />
+          <Keycap position={[0.7, 0.9, 1.4]} texture={4} />
+          <Keycap position={[1.3, -0.3, 2.3]} texture={5} />
+          <Keycap position={[0, 1, 2]} texture={6} />
+          <Keycap position={[-0.7, 0.6, 2]} texture={7} />
+          <Keycap position={[-0.77, 0.1, 2.8]} texture={8} />
+          <Keycap position={[2, 0, 1]} texture={7} />
+        </group>
+      </group>
+
+      <Environment
+        files={["/hdr/blue-studio.hdr"]}
+        environmentIntensity={0.05}
       />
-      <ambientLight intensity={0.2} />
       <spotLight
         position={[-2, 1.5, 3]}
         intensity={30}
